47c34106e13abfe64a4181b04ef3ad5e4b35a597
kissste <steve@halooo.com>
2015-06-13 21:35:12 -0400

    cleanup

--- ESP8266-Arduino-WifiSniffer.ino
+++ ESP8266-Arduino-WifiSniffer.ino
@@ -15,33 +15,61 @@ extern "C" {
 
 #define user_procTaskPrio        0
 #define user_procTaskQueueLen    1
-#define CHANNEL_HOP_INTERVAL 5000
+#define CHANNEL_HOP_INTERVAL   5000
 
-os_event_t    user_procTaskQueue[user_procTaskQueueLen];
+//os_event_t    user_procTaskQueue[user_procTaskQueueLen];
 static os_timer_t channelHop_timer;
 
 //static void loop(os_event_t *events);
 static void promisc_cb(uint8 *buf, uint16 len);
 
-#define printmac(buf, i) os_printf("\t%02X:%02X:%02X:%02X:%02X:%02X", buf[i+0], buf[i+1], buf[i+2], \
-                                                   buf[i+3], buf[i+4], buf[i+5])
+#define printmac(buf, i) Serial.print(buf[i+0],HEX); Serial.print(":"); Serial.print(buf[i+1],HEX); Serial.print(":"); Serial.print(buf[i+2],HEX); Serial.print(":"); Serial.print(buf[i+3],HEX); Serial.print(":"); Serial.print(":"); Serial.print(buf[i+4],HEX); Serial.print(":"); Serial.print(buf[i+5],HEX);
+
+void PrintHex83(uint8_t *data, uint8_t start, uint8_t length) // prints 8-bit data in hex
+{
+   char tmp[length*2+1];
+   byte first;
+   int j=0;
+   for (uint8_t i=0; i<length; i++) 
+   {
+     first = (data[i+start] >> 4) | 48;
+     if (first > 57) tmp[j] = first + (byte)39;
+     else tmp[j] = first ;
+     j++;
+    
+     first = (data[i+start] & 0x0F) | 48;
+     if (first > 57) tmp[j] = first + (byte)39; 
+     else tmp[j] = first;
+     j++;
+   }
+   tmp[length*2] = 0;
+   Serial.print(tmp);
+}
 
 void channelHop(void *arg)
 {
-    // 1 - 13 channel hopping
-    uint8 new_channel = wifi_get_channel() % 12 + 1;
-    os_printf("** hop to %d **\n", new_channel);
-    wifi_set_channel(new_channel);
+  // 1 - 13 channel hopping
+  uint8 new_channel = wifi_get_channel() % 12 + 1;
+  Serial.print("** hop to ");
+  Serial.println(new_channel);
+  wifi_set_channel(new_channel);
 }
 
 static void ICACHE_FLASH_ATTR
 promisc_cb(uint8 *buf, uint16 len)
 {
-    os_printf("-> %3d: %d", wifi_get_channel(), len);
-    printmac(buf,  4);
-    printmac(buf, 10);
-    printmac(buf, 16);
-    os_printf("\n");
+    Serial.print("-> ");
+    Serial.print(wifi_get_channel(), DEC);
+    Serial.print(",");
+    PrintHex83(buf, 10, 6);
+    //printmac(buf, 10); //source mac
+    Serial.print(" -> ");
+    PrintHex83(buf, 4, 6);
+    //printmac(buf,  4); //destination mac
+    Serial.print(" (");
+    PrintHex83(buf, 16, 6);
+    //printmac(buf, 16);
+    Serial.println(")");
 }
 
 //Init function 
@@ -50,27 +78,27 @@ void setup ()
     Serial.begin(115200);
     delayMicroseconds(100);
 
-    Serial.println("*** Monitor mode test ***\r\n");
+    Serial.println("*** Monitor mode test ***");
     
-    Serial.println(" -> Promisc mode setup ... ");
+    Serial.print(" -> Promisc mode setup ... ");
     wifi_set_promiscuous_rx_cb(promisc_cb);
     wifi_promiscuous_enable(1);
-    Serial.println("done.\n");
+    Serial.println("done.");
 
-    Serial.println(" -> Timer setup ... ");
+    Serial.print(" -> Timer setup ... ");
     os_timer_disarm(&channelHop_timer);
     os_timer_setfn(&channelHop_timer, (os_timer_func_t *) channelHop, NULL);
     os_timer_arm(&channelHop_timer, CHANNEL_HOP_INTERVAL, 1);
     Serial.println("done.\n");
     
-    Serial.println(" -> Set opmode ... ");
+    Serial.print(" -> Set opmode ... ");
     wifi_set_opmode( 0x1 );
-    Serial.println("done.\n");
+    Serial.println("done.");
 
     //Start os task
     //system_os_task(loop, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
 
-    Serial.println(" -> Init finished!\n\n");
+    Serial.println(" -> Init finished!");
 }
 
 void loop() {
